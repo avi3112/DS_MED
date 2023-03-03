4885f791f5c3c610722a341d177a2043192d7446
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-03-03 19:06:41 +0530

    new commit

--- PeakElement.js
+++ PeakElement.js
@@ -7,5 +7,6 @@
 
 // Input: nums = [1,2,1,3,5,6,4]
 // Output: 5
+
 // Explanation: Your function can return either index number 1 where the peak element is 2, or 
 //index number 5 where the peak element is 6
--- ProductSubarray.js
+++ ProductSubarray.js
@@ -3,6 +3,9 @@
 // Input: nums = [2,3,-2,4]
 // Output: 6
 // Explanation: [2,3] has the largest product 6.
+
+
+
 // Example 2:
 
 // Input: nums = [-2,0,-1]
