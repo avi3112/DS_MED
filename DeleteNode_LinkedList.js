b7c48ff1ebfd05db6d4813f913103c5e9c284b1d
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-03-04 18:52:13 +0530

    rotate_array_update

--- DeleteNode_LinkedList.js
+++ DeleteNode_LinkedList.js
@@ -3,3 +3,13 @@
 // Explanation: You are given the second node with value 5, the linked list should become 4 ->
 // 1 -> 9 after calling your function.
 
+const test = function(array,index){
+    for(let i=0;i<array.length;i++){
+        if([i]===index){
+            delete(array[i])
+        }
+    }return array
+}
+
+const result= test([4,5,1,9],0)
+console.log(result)
--- RotateArray.js
+++ RotateArray.js
@@ -7,6 +7,38 @@
 // rotate 2 steps to the right: [6,7,1,2,3,4,5]
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-const test= fruits.reverse();
-console.log(test)
+
+
+const rotate = function(nums,k) {
+    k = k % nums.length // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
+    console.log(k)
+    let reverse = function(start,end){
+        while(start < end){
+         let temp = nums[start]  // temp will get 1 value
+         nums[start] = nums[end]   // start will get 7 here
+         nums[end] = temp          // end will get 1 gere
+         start++
+         end--
+     }
+
+    } // suppose  ----->---> 
+    reverse(0, nums.length-1); // reverse   <--<------
+    reverse(0, k-1); // reverse first part ---><----
+    reverse(k, nums.length-1);// reverse second part --->----->
+    return nums
+    
+     
+ };
+
+const result= rotate([1,2,3,4,5,6,7],3)
+console.log(result)
+
+// let see this is the array [1,2,3,4,5,6,7]
+
+// first we will reverse all the array [7,6,5,4,3,2,1]
+
+// second we will reverse the k part [5,6,7,4,3,2,1]
+
+//thirt we will reverse second part [5,6,7,1,2,3,4]
+
+
